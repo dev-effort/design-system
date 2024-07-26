0b20dfde46b621120374076ee433963b325e2081
seungjunkim <dregonc89@gmail.com>
2024-07-26 23:50:26 +0900

    chore: 초기 셋팅 중

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,27 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+import dts from 'vite-plugin-dts';
+import path from 'path';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
-})
+  plugins: [react(), dts({ exclude: ['src/stories/**'] })],
+  build: {
+    lib: {
+      entry: path.resolve(__dirname, 'src/main.tsx'),
+      fileName: format => `index.${format}.js`,
+      name: 'good-design-system',
+    },
+    rollupOptions: {
+      external: ['react', 'react-dom'],
+      output: {
+        globals: {
+          react: 'React',
+          'react-dom': 'ReactDOM',
+        },
+      },
+    },
+    sourcemap: true,
+    emptyOutDir: true,
+  },
+});
