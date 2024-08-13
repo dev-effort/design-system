0577df79d1f2fc9dd03f3f70dd5a519e944c9540
seungjunkim <dregonc89@gmail.com>
2024-08-13 23:17:44 +0900

    feat: button 기능 개발 중

--- src/components/buttons/Button.tsx
+++ src/components/buttons/Button.tsx
@@ -11,16 +11,20 @@ type ButtonProps = {
 
 type Props = PropsWithChildren<ButtonProps> & React.ButtonHTMLAttributes<HTMLButtonElement>;
 
-export const Button = ({ variant, size, ...props }: Props) => {
-  console.log('size', size);
+export const Button = ({ variant, size, children, type = 'button', ...props }: Props) => {
   return (
-    <Container variant={variant} size={size}>
-      하이7
+    <Container variant={variant} size={size} type={type} {...props}>
+      {children}
     </Container>
   );
 };
 
-const Container = styled.div<Required<ButtonProps>>`
+const Container = styled.button<Required<ButtonProps>>`
+  display: inline-flex;
+  align-items: center;
+  justify-content: center;
+  padding: 0px 6px;
+  background-color: ${props => props.theme.colors.color.primary};
   height: ${props => props.theme.size[props.size]}px;
-  border: 1px solid red;
+  ${props => props.variant === 'border' && `border: 1px solid ${props.theme.colors.border.primary}`};
 `;
