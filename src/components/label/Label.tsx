8d72f9251494c5d761367cb0ebeb31df44639537
seungjunkim <dregonc89@gmail.com>
2024-08-21 23:43:03 +0900

    fix: Label htmlattributes 잘못 선언된 것 수정

--- src/components/label/Label.tsx
+++ src/components/label/Label.tsx
@@ -18,7 +18,7 @@ type LabelProps = {
   fullWidth?: boolean;
 };
 
-type Props = LabelProps & Omit<React.ButtonHTMLAttributes<HTMLDivElement>, 'label'>;
+type Props = LabelProps & Omit<React.HTMLAttributes<HTMLDivElement>, 'label'>;
 
 export const Label = forwardRef<HTMLDivElement, Props>(
   (
